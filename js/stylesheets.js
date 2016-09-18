51106c69a1d2c8f50b8d141f44a500302e002c2d
kuladeep93 <s.kuladeep93@gmail.com>
2016-09-18 19:31:40 +0530

    inital structure base template completed few pendings

--- js/stylesheets.js
+++ js/stylesheets.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function () {
 
 
+    $(".first-sub-section").mouseenter(function () {
+        $('.first-sub-section').addClass('.is-hovered');
+        console.log("mouse entered");
+    }).mouseleave(function () {
+        $('.first-sub-section').removeClass('.is-hovered');
+        console.log("mouse exited");
+    });
+
+
+
+});
